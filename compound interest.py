848b244d8fb929bce16b8058004ab03b6007816c
Dhruv Garg <60261904+akuma-Dhruv@users.noreply.github.com>
2020-01-29 23:45:11 +0530

    Add files via upload

---
+++ compound interest.py
@@ -0,0 +1,12 @@
+import os
+print ('program for compound interest ')
+p = input('\nenter principal amount ')
+r= input('\nenter rate of interest')
+n= input ('\nenter no of times interest charged in a year')
+t= input('\nenter  time ')
+rn =int(r)/int(n)
+nt=int(n)*int(t)
+inn= int(p)*pow(1+(float(rn)/100),int(nt))
+print(inn)
+os.system('pause')
+
