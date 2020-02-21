720d8aba31ae0c05dc4cc7040f6e402da630d252
Dhruv Garg <60261904+akuma-Dhruv@users.noreply.github.com>
2020-02-21 22:21:58 +0530

    Add files via upload

---
+++ Factoral.py
@@ -0,0 +1,10 @@
+#import.os
+print("program for factorial of a number")
+n = int(input("\nenter number for factorial"))
+
+r = n-1
+while r>1 :
+ n*=r;
+ r-=1;
+print ('factorial of number is ',n)
+#os.system("pause")
